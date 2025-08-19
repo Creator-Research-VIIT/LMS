153e68d7d10bcb95b748a487e86d095af10ff05c
DevAyush075 <153929909+DevAyush075@users.noreply.github.com>
2025-08-19 11:16:13 +0530

    Your commit message here

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { AboutSection } from "@/components/about-section"
 import { Testimonials } from "@/components/testimonials"
 import { ContactSection } from "@/components/contact-section"
 import { Footer } from "@/components/footer"
+import { AwardsSection } from "@/components/awards"
 
 export default function HomePage() {
   return (
@@ -15,6 +16,7 @@ export default function HomePage() {
         <HeroSection />
         <NewCourses />
         <ExploreCourses />
+        <AwardsSection />
         <AboutSection />
         <Testimonials />
         <ContactSection />
